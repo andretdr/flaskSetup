f22f781e17855d7dc8dd994fd762a710e79069e3
andretdr <andre@rocketmail.com>
2024-05-02 12:29:27 +0800

    trying mySQL instead

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request #flask import
 from random import randrange
-import mariadb
+import mysql.connector
 import sys
 
 try:
-    conn = mariadb.connect(
+    conn = mysql.connector.connect(
         user="d3xj7d753lhx14ad",
         password="qyjau9ud4manbl1w",
         host="z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port=3306,
         database="nodabg0vdbkgxoop"
     )
-except mariadb.Error as e:
+except mysql.connector.Error as e:
     print(f"Error connecting to MariaDB platform: {e}")
     sys.exit(1)
 
