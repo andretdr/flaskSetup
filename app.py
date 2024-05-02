9b9e5b992855e46db512483aecf1f48aafdb89a9
andretdr <andre@rocketmail.com>
2024-05-02 12:46:02 +0800

    adding mysql-connector-python

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request #flask import
 from random import randrange
-
+import mysql.connector
 import sys
+
+try:
+    conn = mysql.connector.connect(
+        user="d3xj7d753lhx14ad",
+        password="qyjau9ud4manbl1w",
+        host="z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port=3306,
+        database="nodabg0vdbkgxoop"
+    )
+except mysql.connector.Error as e:
+    print(f"Error connecting to MariaDB platform: {e}")
+    sys.exit(1)
+
+db = conn.cursor(dictionary=True)
 
 app = Flask(__name__) # to initiate this for web app
 
 @app.route("/")
 def index():
-    randindex = randrange(2)
+    db.execute("SELECT * FROM hellos;")
+    rows = db.fetchall()
+    randindex = randrange(len(rows))
 
-    return render_template("index.html", status = "asdmessage")
+    return render_template("index.html", status = rows[randindex]['message'])
