ccd50224206acc35dd06d253824a154df9649c72
andretdr <andre@rocketmail.com>
2024-05-02 12:38:25 +0800

    trying without db

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 from flask import Flask, render_template, request #flask import
 from random import randrange
-from mysql import connector
+
 import sys
-
-try:
-    conn = connector.connect(
-        user="d3xj7d753lhx14ad",
-        password="qyjau9ud4manbl1w",
-        host="z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        port=3306,
-        database="nodabg0vdbkgxoop"
-    )
-except connector.Error as e:
-    print(f"Error connecting to MariaDB platform: {e}")
-    sys.exit(1)
-
-db = conn.cursor(dictionary=True)
 
 app = Flask(__name__) # to initiate this for web app
 
 @app.route("/")
 def index():
-    db.execute("SELECT * FROM hellos;")
-    rows = db.fetchall()
-    randindex = randrange(len(rows))
+    randindex = randrange(2)
 
-    return render_template("index.html", status = rows[randindex]['message'])
+    return render_template("index.html", status = "asdmessage")
